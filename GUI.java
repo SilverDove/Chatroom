97886c8deef3ebb12e16da2afbef1dbdbd9798bf
SilverDove <tricot.clara99@gmail.com>
2020-05-23 10:19:23 +0200

    [features][database+GUI] Add error message + clean textArea

--- GUI.java
+++ GUI.java
@@ -10,6 +10,7 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -20,12 +21,13 @@ public class GUI implements ActionListener {
 	String username = null, firstname = null, lastname = null, password = null;
 	User newUser;
 	DatabaseSQLite db;
-	
+	boolean flag;	
 	
 	//Creation of frames
 	JFrame principal_frame = new JFrame("Chat GUI");
 	JFrame new_account_frame = new JFrame("New account");
 	JFrame connection_frame = new JFrame("Log in");
+	JFrame error_frame = new JFrame("Error");
 	
 	//Creation of panels
 	JPanel accountPanel = new JPanel();
@@ -53,7 +55,7 @@ public class GUI implements ActionListener {
 	
 	public GUI(){
 		
-	db = DatabaseSQLite.getInstance();//get the database
+	db = DatabaseSQLite.getInstance();//get the instance of the database
 		
 	//Connection frame initialization
 	connection_frame.setMinimumSize(new Dimension(500, 100));
@@ -157,13 +159,15 @@ public class GUI implements ActionListener {
 		case "new":
 			multiPanel.setBorder(BorderFactory.createTitledBorder("Contacts"));
 			break;
-		case "send":
+		case "send"://Send a message in the chat
 			String message = message_textArea.getText();
 			JLabel new_message = new JLabel(message);
 			messagesPanel.add(new_message);
 			messagesPanel.updateUI();
 			message_textArea.setText(null);
-			//TODO Add the message to the database
+			
+			//TODO: Save message into the database	
+			
 			break;
 		case "login"://Log in the Chatroom
 			//Get information from textArea
@@ -171,14 +175,21 @@ public class GUI implements ActionListener {
 			password = password_textArea.getText();
 			
 			//Check if the user exists in the database
-			db.logIn(username,password);
+			flag = db.logIn(username,password);
 			
-			//GUI
-			connection_frame.setVisible(false);
-			principal_frame.setLocationRelativeTo(null);
-			principal_frame.pack();
-			principal_frame.setVisible(true);
-			
+			if(flag == false) {//Wrong password/usernage
+				JOptionPane.showMessageDialog(error_frame, "Your password/username is incorrect. Please try again", "Error", JOptionPane.ERROR_MESSAGE);
+			}else {//Correct information about the user
+				//Display the chatroom
+				connection_frame.setVisible(false);
+				principal_frame.setLocationRelativeTo(null);
+				principal_frame.pack();
+				principal_frame.setVisible(true);
+			}
+			//Clear the contents of the textArea
+			username_textArea.setText(null);
+			password_textArea.setText(null);
+					
 			break;
 			
 		case "new_account":
@@ -186,6 +197,7 @@ public class GUI implements ActionListener {
 			new_account_frame.setLocationRelativeTo(null);
 			new_account_frame.pack();
 			new_account_frame.setVisible(true);
+			
 			break;
 			
 		case "create"://Add account to database
@@ -197,7 +209,13 @@ public class GUI implements ActionListener {
 			
 			//Save new user into the database
 			newUser = new User(username,firstname,lastname,password);//create user
-			db.CreateAccount(newUser);//add the new user into the database		
+			db.CreateAccount(newUser);//add the new user into the database
+			
+			//Clear the contents of the textArea
+			new_username_textArea.setText(null);
+			new_firstname_textArea.setText(null);
+			new_lastname_textArea.setText(null);
+			new_password_textArea.setText(null);
 			
 			//GUI
 			new_account_frame.setVisible(false);
